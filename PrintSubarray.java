fd79d6c196c152386241a872ee3ffd15122737a7
MayuriPawar25 <110191801+MayuriPawar25@users.noreply.github.com>
2023-06-04 23:14:52 +0530

    Update PrintSubarray.java

--- PrintSubarray.java
+++ PrintSubarray.java
@@ -4,24 +4,31 @@ public class PrintSubarray
 	public static void PrintSubarray(int arr[])
 	{
 		int totsubarray=0;
+		int minSum=Integer.MAX_VALUE;
+		int maxSum=Integer.MIN_VALUE;
 		for(int i=0; i<arr.length; i++)
 		{
 			int start=i;
 			int sum=0;
+			
 			for(int j=i; j<arr.length; j++)
 			{
 				int end=j;
-				sum+=arr[j];
+				sum += arr[j];
+				minSum=Math.min(minSum,sum);
+				maxSum=Math.max(maxSum,sum);
 				for(int k=start; k<=end; k++)
 				{
 					System.out.print(arr[k]+" ");
 				}
-				totsubarray++;
-				System.out.println("  =  "+sum);   //print the sum of subarray
+				totsubarray++;   //n(n+1)/2
+				System.out.println("  =  "+sum);
 			}
 			System.out.println();
 		}
 		System.out.println("Total Subarrays: "+totsubarray);
+		System.out.println("Minimum sum: "+minSum);
+		System.out.println("Maximum sum: "+maxSum);
 	}
 	public static void main(String[] args)
 	{
@@ -39,3 +46,4 @@ public class PrintSubarray
 		
 	}
 }
+
